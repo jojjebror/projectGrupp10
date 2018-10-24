192317e31293179729fb107acc55f34bb2e1e01a
johan <jjohaan@live.se>
2018-10-24 14:33:18 +0200

    johan

---
+++ form1/form1/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace form1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnKategorierTaBort = new System.Windows.Forms.Button();
+            this.btnKategorierLaggTill = new System.Windows.Forms.Button();
+            this.tbKategorier = new System.Windows.Forms.TextBox();
+            this.lblKategorier = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.lblAvsnitt = new System.Windows.Forms.Label();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.btnTaBort = new System.Windows.Forms.Button();
+            this.btnSpara = new System.Windows.Forms.Button();
+            this.btnSok = new System.Windows.Forms.Button();
+            this.lblKategori = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.lblFrekvens = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lblUrl = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lbKategorier = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnKategorierTaBort
+            // 
+            this.btnKategorierTaBort.Location = new System.Drawing.Point(813, 350);
+            this.btnKategorierTaBort.Name = "btnKategorierTaBort";
+            this.btnKategorierTaBort.Size = new System.Drawing.Size(78, 23);
+            this.btnKategorierTaBort.TabIndex = 40;
+            this.btnKategorierTaBort.Text = "Ta Bort..";
+            this.btnKategorierTaBort.UseVisualStyleBackColor = true;
+            this.btnKategorierTaBort.Click += new System.EventHandler(this.btnKategorierTaBort_Click);
+            // 
+            // btnKategorierLaggTill
+            // 
+            this.btnKategorierLaggTill.Location = new System.Drawing.Point(732, 350);
+            this.btnKategorierLaggTill.Name = "btnKategorierLaggTill";
+            this.btnKategorierLaggTill.Size = new System.Drawing.Size(75, 23);
+            this.btnKategorierLaggTill.TabIndex = 39;
+            this.btnKategorierLaggTill.Text = "Lägg Till";
+            this.btnKategorierLaggTill.UseVisualStyleBackColor = true;
+            this.btnKategorierLaggTill.Click += new System.EventHandler(this.btnKategorierLaggTill_Click);
+            // 
+            // tbKategorier
+            // 
+            this.tbKategorier.Location = new System.Drawing.Point(648, 309);
+            this.tbKategorier.Name = "tbKategorier";
+            this.tbKategorier.Size = new System.Drawing.Size(358, 20);
+            this.tbKategorier.TabIndex = 36;
+            // 
+            // lblKategorier
+            // 
+            this.lblKategorier.AutoSize = true;
+            this.lblKategorier.Location = new System.Drawing.Point(648, 6);
+            this.lblKategorier.Name = "lblKategorier";
+            this.lblKategorier.Size = new System.Drawing.Size(55, 13);
+            this.lblKategorier.TabIndex = 35;
+            this.lblKategorier.Text = "Kategorier";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(648, 388);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(358, 155);
+            this.richTextBox1.TabIndex = 33;
+            this.richTextBox1.Text = "";
+            // 
+            // lblAvsnitt
+            // 
+            this.lblAvsnitt.AutoSize = true;
+            this.lblAvsnitt.Location = new System.Drawing.Point(13, 369);
+            this.lblAvsnitt.Name = "lblAvsnitt";
+            this.lblAvsnitt.Size = new System.Drawing.Size(39, 13);
+            this.lblAvsnitt.TabIndex = 32;
+            this.lblAvsnitt.Text = "Avsnitt";
+            // 
+            // listView2
+            // 
+            this.listView2.Location = new System.Drawing.Point(13, 388);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(597, 155);
+            this.listView2.TabIndex = 31;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.List;
+            // 
+            // btnTaBort
+            // 
+            this.btnTaBort.Location = new System.Drawing.Point(534, 307);
+            this.btnTaBort.Name = "btnTaBort";
+            this.btnTaBort.Size = new System.Drawing.Size(75, 23);
+            this.btnTaBort.TabIndex = 30;
+            this.btnTaBort.Text = "Ta Bort...";
+            this.btnTaBort.UseVisualStyleBackColor = true;
+            // 
+            // btnSpara
+            // 
+            this.btnSpara.Location = new System.Drawing.Point(421, 308);
+            this.btnSpara.Name = "btnSpara";
+            this.btnSpara.Size = new System.Drawing.Size(75, 23);
+            this.btnSpara.TabIndex = 29;
+            this.btnSpara.Text = "Spara";
+            this.btnSpara.UseVisualStyleBackColor = true;
+            // 
+            // btnSok
+            // 
+            this.btnSok.Location = new System.Drawing.Point(312, 308);
+            this.btnSok.Name = "btnSok";
+            this.btnSok.Size = new System.Drawing.Size(75, 23);
+            this.btnSok.TabIndex = 28;
+            this.btnSok.Text = "Sök";
+            this.btnSok.UseVisualStyleBackColor = true;
+            // 
+            // lblKategori
+            // 
+            this.lblKategori.AutoSize = true;
+            this.lblKategori.Location = new System.Drawing.Point(303, 227);
+            this.lblKategori.Name = "lblKategori";
+            this.lblKategori.Size = new System.Drawing.Size(49, 13);
+            this.lblKategori.TabIndex = 27;
+            this.lblKategori.Text = "Kategori:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(306, 243);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 26;
+            // 
+            // lblFrekvens
+            // 
+            this.lblFrekvens.AutoSize = true;
+            this.lblFrekvens.Location = new System.Drawing.Point(176, 226);
+            this.lblFrekvens.Name = "lblFrekvens";
+            this.lblFrekvens.Size = new System.Drawing.Size(114, 13);
+            this.lblFrekvens.TabIndex = 25;
+            this.lblFrekvens.Text = "Uppdateringsfrekvens:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(179, 243);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 24;
+            // 
+            // lblUrl
+            // 
+            this.lblUrl.AutoSize = true;
+            this.lblUrl.Location = new System.Drawing.Point(10, 228);
+            this.lblUrl.Name = "lblUrl";
+            this.lblUrl.Size = new System.Drawing.Size(32, 13);
+            this.lblUrl.TabIndex = 23;
+            this.lblUrl.Text = "URL:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 243);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(161, 20);
+            this.textBox1.TabIndex = 22;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(598, 192);
+            this.listView1.TabIndex = 21;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Namn";
+            this.columnHeader1.Width = 183;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Frekvens";
+            this.columnHeader2.Width = 203;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Kategori";
+            this.columnHeader3.Width = 193;
+            // 
+            // lbKategorier
+            // 
+            this.lbKategorier.FormattingEnabled = true;
+            this.lbKategorier.Location = new System.Drawing.Point(651, 39);
+            this.lbKategorier.Name = "lbKategorier";
+            this.lbKategorier.Size = new System.Drawing.Size(355, 251);
+            this.lbKategorier.TabIndex = 41;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1048, 599);
+            this.Controls.Add(this.lbKategorier);
+            this.Controls.Add(this.btnKategorierTaBort);
+            this.Controls.Add(this.btnKategorierLaggTill);
+            this.Controls.Add(this.tbKategorier);
+            this.Controls.Add(this.lblKategorier);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.lblAvsnitt);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.btnTaBort);
+            this.Controls.Add(this.btnSpara);
+            this.Controls.Add(this.btnSok);
+            this.Controls.Add(this.lblKategori);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.lblFrekvens);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lblUrl);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKategorierTaBort;
+        private System.Windows.Forms.Button btnKategorierLaggTill;
+        private System.Windows.Forms.TextBox tbKategorier;
+        private System.Windows.Forms.Label lblKategorier;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label lblAvsnitt;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.Button btnTaBort;
+        private System.Windows.Forms.Button btnSpara;
+        private System.Windows.Forms.Button btnSok;
+        private System.Windows.Forms.Label lblKategori;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label lblFrekvens;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label lblUrl;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ListBox lbKategorier;
+    }
+}
+
---
+++ form1/form1/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace form1
+{
+    public partial class Form1 : Form
+    {
+        List<string> listaKategorier;
+        public Form1()
+        {
+            InitializeComponent();
+            this.listaKategorier = new List<string>();
+        }
+
+        private void LaggTillKategorier()
+        {
+            lbKategorier.Items.Clear();
+            
+
+            foreach(var item in listaKategorier)
+            {
+                lbKategorier.Items.Add(item);
+            }
+            
+
+
+        }
+
+
+
+        private void btnKategorierLaggTill_Click(object sender, EventArgs e)
+        {
+            listaKategorier.Add(tbKategorier.Text);
+            tbKategorier.Clear();
+            LaggTillKategorier();
+            
+        }
+
+        private void btnKategorierTaBort_Click(object sender, EventArgs e)
+        {
+            
+            if (lbKategorier.Items.Count>0)
+            {
+                
+                lbKategorier.Items.Remove(lbKategorier.SelectedItems[0]);
+                
+            }
+        }
+        
+    }
+
+
+}
